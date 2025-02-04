994fab3c49c438720a3ed00f0fbd72affc6e3275
Phoenix <livingrainbow100@gmail.com>
2025-02-03 22:07:26 -0500

    find_duplicates_one_set function added to find_duplicates.py

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -10,6 +10,18 @@ def find_duplicates_nested_loop(l: list) -> list:
     
     return list(s)
 
+def find_duplicates_one_set(l: list) -> list:
+    d=dict()
+    length=len(l)
+    duplicates=list()
+
+    for i in range(0,length):
+        d[l[i]]=d.get(l[i],0)+1
+
+        if d[l[i]]==2:
+            duplicates.append(l[i])
+    
+    return duplicates
 
 if __name__=="__main__":
     sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
@@ -21,3 +33,10 @@ if __name__=="__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+
+    print()
+
+    print("Sample 1:", find_duplicates_one_set(sample1))
+    print("Sample 2:", find_duplicates_one_set(sample2))
+    print("Sample 3:", find_duplicates_one_set(sample3))
+    print("Sample 4:", find_duplicates_one_set(sample4))
